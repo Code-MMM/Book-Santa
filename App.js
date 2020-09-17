fea918e0925838505bc583fd099ba9c0d70d5766
Code-MMM <63055277+Code-MMM@users.noreply.github.com>
2020-09-17 16:27:20 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {CreateAppContainer, CreateSwitchNavigator} from "react-navigation"
+import {createAppContainer, createSwitchNavigator} from "react-navigation"
 import LoginScreen from "./screens/Login"
+import DonationScreen from './screens/Donation';
+import RequestScreen from "./screens/Request"
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LoginScreen></LoginScreen>
+      <AppContainer></AppContainer>
     </View>
   );
 }
 
+var AppContainer = createAppContainer(SwitchNavigator)
 
+var SwitchNavigator = createSwitchNavigator({
+  LoginScreen:{screen:LoginScreen},
+  DonationScreen:{screen:DonationScreen},
+  RequestScreen:{screen:RequestScreen},
+})
+
+const TabNavigator = createBottomTabNavigator({
+  Request: {screen: RequestScreen},
+  Donation: {screen: DonationScreen},
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "Donation"){
+        return(
+          <Image
+          source={require("./assets/donation.jpg")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "Request"){
+        return(
+          <Image
+          source={require("./assets/request.jpg")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
+}
+);
 
 const styles = StyleSheet.create({
   container: {
